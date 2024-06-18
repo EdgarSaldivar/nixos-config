9b5ae2bc5db013e500cd84e7197b6d8e6e610635
EdgarSaldivar <edgarls93@hotmail.com>
2024-06-18 14:11:39 -0700

    seperated the flakes from host

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nixpkgs, ... }: {
+    nixosConfigurations = {
+      pelargir = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./hosts/pelargir/ inputs.disko.nixosModules.disko ];
+      };
+      minas-tirith = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./hosts/minas-tirith inputs.disko.nixosModules.disko ];
+      };
+      # add more hosts as needed
+    };
+  };
+}
