167a56bb8dde6687fae21df14c6369b175a0d6e4
EdgarSaldivar <edgarls93@hotmail.com>
2024-06-19 13:25:36 -0700

    syntax

--- users/edgar.nix
+++ users/edgar.nix
@@ -8,7 +8,7 @@
          extraGroups = [ "wheel" "networkmanager" ];
          #shell = lib.mkDefault "/run/current-system/sw/bin/bash";
          openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA+PoI3q67ZKz5oWtHVWfKzIRyBagoaFqYu/TqndfqTW" ];
-         hashedPassword = [ "$y$j9T$ET2aGnxdPcazW7JWBfPfo0$0nAEs/cggVEG1abKv98SlTl7FcVOml1SiCLjpA3Odj9" ];
+         hashedPassword = "$y$j9T$ET2aGnxdPcazW7JWBfPfo0$0nAEs/cggVEG1abKv98SlTl7FcVOml1SiCLjpA3Odj9";
 };
 };
 }
