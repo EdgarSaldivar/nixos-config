400226084e8d3f88cab0bc90b94a1836439869b8
EdgarSaldivar <edgarls93@hotmail.com>
2024-06-20 15:37:32 -0700

    synatx

--- users/edgar/default.nix
+++ users/edgar/default.nix
@@ -1,7 +1,7 @@
 { config, lib, ... }:
 
 {
-  nixos.sops.secrets."users/edgar/hashedPassword" = {
+  nix.sops.secrets."users/edgar/hashedPassword" = {
     sopsFile = ./secrets.enc.yaml;
     neededForUsers = true;
   };
@@ -13,5 +13,6 @@
     extraGroups = [ "wheel" "networkmanager" ];
     #shell = lib.mkDefault "/run/current-system/sw/bin/bash";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA+PoI3q67ZKz5oWtHVWfKzIRyBagoaFqYu/TqndfqTW" ];
+    hashedPassword = "$6$B.UffRI3k5R278aQ$v4I9eQJx81fhab/Hz3hJbmB.JOU6Zyr.o9yMXllp4oTkA.bZgLU9jEIJiC/3VTCYnWYmYuWfVYPg5MrsrL9KF/";
   };
 }
