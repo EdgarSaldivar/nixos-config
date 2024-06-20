6dc5d86ffd42b54935b7e96bae44057aa3e10b7b
EdgarSaldivar <edgarls93@hotmail.com>
2024-06-20 15:33:09 -0700

    Fixed Syntax

--- users/edgar/default.nix
+++ users/edgar/default.nix
@@ -1,23 +1,20 @@
 { config, lib, ... }:
 
 {
-  config = {
-    linux = {
-      sops.secrets."users/edgar/hashedPassword" = {
-        sopsFile = ./secrets.enc.yaml;
-        neededForUsers = true;
-      };
+  linux = {
+    sops.secrets."users/edgar/hashedPassword" = {
+      sopsFile = ./secrets.enc.yaml;
+      neededForUsers = true;
     };
   };
-  users ={
-        users.edgar = {
-         isNormalUser = true;
-         home = "/home/edgar";
-         description = "Edgar Saldivar";
-         extraGroups = [ "wheel" "networkmanager" ];
-         #shell = lib.mkDefault "/run/current-system/sw/bin/bash";
-         openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA+PoI3q67ZKz5oWtHVWfKzIRyBagoaFqYu/TqndfqTW" ];
-         hashedPassword = "$6$B.UffRI3k5R278aQ$v4I9eQJx81fhab/Hz3hJbmB.JOU6Zyr.o9yMXllp4oTkA.bZgLU9jEIJiC/3VTCYnWYmYuWfVYPg5MrsrL9KF/";
-};
-};
+
+  users.users.edgar = {
+    isNormalUser = true;
+    home = "/home/edgar";
+    description = "Edgar Saldivar";
+    extraGroups = [ "wheel" "networkmanager" ];
+    #shell = lib.mkDefault "/run/current-system/sw/bin/bash";
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA+PoI3q67ZKz5oWtHVWfKzIRyBagoaFqYu/TqndfqTW" ];
+    hashedPassword = "$6$B.UffRI3k5R278aQ$v4I9eQJx81fhab/Hz3hJbmB.JOU6Zyr.o9yMXllp4oTkA.bZgLU9jEIJiC/3VTCYnWYmYuWfVYPg5MrsrL9KF/";
+  };
 }
