877b1a7f5e66b842fcb491c81d9e9ce058ce8f9d
EdgarSaldivar <edgarls93@hotmail.com>
2024-06-20 15:35:24 -0700

    Syntax fix

--- users/edgar/default.nix
+++ users/edgar/default.nix
@@ -1,11 +1,9 @@
 { config, lib, ... }:
 
 {
-  linux = {
-    sops.secrets."users/edgar/hashedPassword" = {
-      sopsFile = ./secrets.enc.yaml;
-      neededForUsers = true;
-    };
+  nixos.sops.secrets."users/edgar/hashedPassword" = {
+    sopsFile = ./secrets.enc.yaml;
+    neededForUsers = true;
   };
 
   users.users.edgar = {
@@ -15,6 +13,5 @@
     extraGroups = [ "wheel" "networkmanager" ];
     #shell = lib.mkDefault "/run/current-system/sw/bin/bash";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA+PoI3q67ZKz5oWtHVWfKzIRyBagoaFqYu/TqndfqTW" ];
-    hashedPassword = "$6$B.UffRI3k5R278aQ$v4I9eQJx81fhab/Hz3hJbmB.JOU6Zyr.o9yMXllp4oTkA.bZgLU9jEIJiC/3VTCYnWYmYuWfVYPg5MrsrL9KF/";
   };
 }
