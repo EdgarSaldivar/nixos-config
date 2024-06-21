f7ec444b843c9aa12a8341e6ae56bf34b8acd8a8
EdgarSaldivar <edgarls93@hotmail.com>
2024-06-21 04:35:39 -0700

    switch to hash

--- users/edgar/default.nix
+++ users/edgar/default.nix
@@ -4,8 +4,8 @@ let
   sops = import (pkgs.fetchFromGitHub {
     owner = "Mic92";
     repo = "sops-nix";
-    rev = "797ce4c"; # replace with the commit hash you want to use
-    sha256 = "0f3k3vm6v0c2ld33i5fcfj3mj46z8qpyp7ymf4pbn78h2gy4hvry"; # replace with the correct SHA-256 hash
+    rev = "797ce4c";
+    sha256 = "0f3k3vm6v0c2ld33i5fcfj3mj46z8qpyp7ymf4pbn78h2gy4hvry";
   });
   secrets = sops.secrets."./secrets.enc.yaml".path;
 in
@@ -17,8 +17,8 @@ in
       home = "/home/edgar";
       description = "Edgar Saldivar";
       extraGroups = [ "wheel" "networkmanager" ];
-      #hashedPassword = secrets.users.edgar.hashedPassword;
-      password = "123";
+      hashedPassword = secrets.users.edgar.hashedPassword;
+      #password = "123";
       openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA+PoI3q67ZKz5oWtHVWfKzIRyBagoaFqYu/TqndfqTW" ];
     };
   };
