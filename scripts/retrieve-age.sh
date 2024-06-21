00a7a3090abec2b59a0194a36e4a6c9459f1a727
EdgarSaldivar <edgarls93@hotmail.com>
2024-06-21 01:48:29 -0700

    fixing/testing op

--- scripts/retrieve-age.sh
+++ scripts/retrieve-age.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+  echo $PATH
   # Check if the age.key already exists
   if [ ! -f /etc/nixos/age.key ]; then
   # Use the 1Password CLI to get the age.key
-  eval $(op signin my.1password.com)
-  op get document 'age.key' > /etc/nixos/age.key
+  eval $(/usr/local/bin/op signin my.1password.com)
+  /usr/local/bin/op get document 'age.key' > /etc/nixos/age.key
   chmod 600 /etc/nixos/age.key
   fi
